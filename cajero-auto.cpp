fada53d12d3177ff5deb24d3117c4e3843f0ace7
Yolvan G. Zambrano <yolvangzg@gmail.com>
2022-06-18 08:27:37 -0400

    AutoCajero - Formateando el código ===================================
     - Añadidos espacios y eliminados saltos innecesarios en estructuras de control, llamadas de función etc.
     - Simplificada la lógica en estructura if de función mete-saca() y corregido return de tipo distinto en función extraer().

--- cajero-auto.cpp
+++ cajero-auto.cpp
@@ -5,108 +5,95 @@ Actualizacion: SI SIRVIO XDXDXD.
 
 No usar vectores, 
 Verificar la extracion de datos(3).*/
-#include<iostream>
-#include<fstream>
-#include<string>
-#include<cmath>
-#include<stdlib.h>
-
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <stdlib.h>
 //Para hacer cosas nice xd.
-#include<thread>
-#include<chrono>
+#include <thread>
+#include <chrono>
+
 using namespace std;
-/*-----------------Administracion de Datos------------------*/
 
 string name = "MasterSex", pass = "2684"; float founds = 10.5;
 
-void spam_de_puntos(int intervalo, int cantidad){
-	
-	for(int i = 0; i < cantidad; i++){
-		
-		cout<<".";
-		this_thread::sleep_for(chrono::milliseconds(intervalo));
-	}
-}
-void mostrar_frase_lentamente(int intervalo, string frase){
-	
-	for(int i = 0; i < frase.size(); i++){
-		
-		cout<<frase[i];
+void spam_de_puntos (int intervalo, int cantidad) {
+	for (int i = 0; i < cantidad; i++) {
+		cout << ".";
 		this_thread::sleep_for(chrono::milliseconds(intervalo));
 	}
 }
 
+void mostrar_frase_lentamente (int intervalo, string frase) {
+	for (int i = 0; i < frase.size(); i++) {
+		cout << frase[i];
+		this_thread::sleep_for(chrono::milliseconds(intervalo));
+	}
+}
 
-string extraer(int lugar, string linea){
+/*-----------------Administracion de Datos------------------*/
+string extraer(int lugar, string linea) {
 	
-	string nombre, clave, fondos;char letra;
+	string nombre, clave, fondos;
+	char letra;
 	
-	for(int i = 0; i < linea.size(); i++){
-		
+	for (int i = 0; i < linea.size(); i++) {
 		letra = linea[i];
-		if(letra == '|')break;
+		if (letra == '|') break;
 		nombre += letra;
 	}
 	
-	for(int i = nombre.size() + 1; i < linea.size(); i++){
-		
+	for (int i = nombre.size() + 1; i < linea.size(); i++) {
 		letra = linea[i];
-		if(letra == '|')break;
+		if (letra == '|') break;
 		clave += letra;
 	}
 	
-	for(int i = nombre.size() + clave.size() + 2;i < linea.size(); i++){
-		
+	for (int i = nombre.size() + clave.size() + 2; i < linea.size(); i++) {
 		letra = linea[i];
 		if(letra == '|')break;
 		fondos += letra;
 	}
 	
-//cout<<nombre<<";"<<clave<<";"<<fondos;
-//exit(1);
-	switch(lugar){
-		
+	//cout << nombre << ";" << clave << ";" << fondos;
+	//exit(1);
+	switch (lugar) {
 		case 1: return nombre;
 		case 2: return clave;
 		case 3: return fondos;
-		
-		default: return "No funciono xd.";
 	}
-	return 0;
+	return "No funciono xd.";
 }
 
-bool comprobar(int lugar, string info){
+bool comprobar (int lugar, string info) {
 	
 	string linea;
 	ifstream file("registro.txt");
 	
-	while(getline(file, linea)){
-		
-		if(info == extraer(lugar, linea))return true;
+	while (getline(file, linea)) {
+		if (info == extraer(lugar, linea)) return true;
 	}
-	
 	return false;
 }
 
 
-void mete_saca(bool meter){
+void mete_saca (bool meter) {
 	
 	string linea, respaldo; float monto;
 	ifstream file("registro.txt");
 	
-	if(meter){//Deposito
-			
+	if (meter) {//Deposito
 		spam_de_puntos(20, 20);
 		mostrar_frase_lentamente(20, " +$+ Meter plata +$+ ");
 		spam_de_puntos(20, 20);
 		
-		cout<<endl<<"Monto a depositar: ";
-		cin>>monto;
+		cout << endl << "Monto a depositar: ";
+		cin >> monto;
 		
-		while(getline(file, linea)){
+		while (getline(file, linea)) {
 			
-			if(name == extraer(1, linea)){
-				
+			if (name == extraer(1, linea)) {
 				respaldo += name + "|" + pass + "|" + to_string(founds + monto) + "\n";
 				spam_de_puntos(20, 10);
 				continue;
@@ -115,21 +102,16 @@ void mete_saca(bool meter){
 		}
 		file.close();
 		founds += monto;
-	}
-	
-	if(!meter){//Retiro
-		
+	} else {//Retiro
 		spam_de_puntos(20, 20);
 		mostrar_frase_lentamente(20, " -$- Sacar plata -$- ");
 		spam_de_puntos(20, 20);
 		
-		cout<<endl<<"Monto a retirar: ";
-		cin>>monto;
+		cout << endl << "Monto a retirar: ";
+		cin >> monto;
 		
-		while(getline(file, linea)){
-			
-			if(name == extraer(1, linea)){
-				
+		while (getline(file, linea)) {
+			if (name == extraer(1, linea)) {
 				respaldo += name + "|" + pass + "|" + to_string(founds - monto) + "\n";
 				spam_de_puntos(20, 10);
 				continue;
@@ -141,7 +123,7 @@ void mete_saca(bool meter){
 	}
 	
 	ofstream file2("registro.txt");
-	file2<<respaldo;
+	file2 << respaldo;
 	file2.close();
 	
 }
@@ -158,81 +140,70 @@ int menu_user_logged(){
 	frase += "5) Borrar cuenta.\n";
 	frase += "6) Cerrar sesion.\n";
 	
-	while(true){
+	while (true) {
+		spam_de_puntos(20, 50);
+		cout << endl << "Usuario: " << name << endl << "Clave: " << pass << endl;
+		cout << "Fondos: " << founds << endl;
 		
 		spam_de_puntos(20, 50);
-		cout<<endl<<"Usuario: "<<name<<endl<<"Clave: "<<pass<<endl;
-		cout<<"Fondos: "<<founds<<endl;
-		
-		spam_de_puntos(20, 50);
-		cout<<endl;
+		cout << endl;
 		mostrar_frase_lentamente(30, frase);
-		cin>>select;
+		cin >> select;
 		
-		switch(select){
+		switch (select) {
 			
-			case 1://Depositar dinero.
-				mete_saca(true); break;
-				
-			case 2://Sacar dinero.
-				mete_saca(false); break;
-				
-			case 3://Transferir dinero a otra cuenta.
-			case 4://Cambiar datos.
-			case 5://Borrar cuenta.
+			case 1: mete_saca(true); break;	 //Depositar dinero.
+			case 2: mete_saca(false); break; //Sacar dinero.
+			case 3: break; 					 //Transferir dinero a otra cuenta.
+			case 4: break; 					 //Cambiar datos.
+			case 5: break;					 //Borrar cuenta.
 			case 6: 
 				name = "";
 				pass = "";
-				return 0;
-			
+				return 0;		
 			default:
-				cout<<"Opcion invalida, subnormal."<<endl;
+				cout << "Opcion invalida, subnormal." << endl;
 		}
 	}
 }
 
 
-int crear(){
+int crear () {
 	
 	spam_de_puntos(50, 20);
 	mostrar_frase_lentamente(50, "  @ Crear una Cuenta @  ");
 	spam_de_puntos(50, 20);
-	cout<<endl;
-//exit(1);
+	cout << endl;
+	//exit(1);
 	
 	string respaldo, linea, cuenta_nueva, nombre, clave, conf, fondos;
 	
-	while(true){//Nombre.
-		
-		cout<<"Introduzca un nombre de usuario: ";
-		cin>>nombre;
-		if(comprobar(1, nombre)){
-			
-			cout<<"Nombre invalido."<<endl;
+	while (true) {//Nombre.
+		cout << "Introduzca un nombre de usuario: ";
+		cin >> nombre;
+		if (comprobar(1, nombre)) {
+			cout << "Nombre invalido." << endl;
 			continue;
 		}
 		break;
 	}
-	while(true){//Clave.
-		
-		cout<<"Introduzca una clave: ";
-		cin>>clave;
-		cout<<"Confirmar clave: ";
-		cin>>conf;
+	while (true) {//Clave.
+		cout << "Introduzca una clave: ";
+		cin >> clave;
+		cout << "Confirmar clave: ";
+		cin >> conf;
 		if(conf == clave)break;
-		cout<<"Clave invalida."<<endl;
+		cout << "Clave invalida." << endl;
 	}
-	while(true){//Fondos
-		
-		cout<<"Monto a abonar: ";
-		cin>>fondos;
+	while (true) {//Fondos
+		cout << "Monto a abonar: ";
+		cin >> fondos;
 		if(stof(fondos) > 0)break;
-		cout<<"Monto invalido."<<endl;
+		cout << "Monto invalido." << endl;
 	}
 	
 	ifstream file("registro.txt");
 	while(getline(file, linea)){
-		
 		respaldo += linea + "\n";
 		
 		spam_de_puntos(10, 3);
@@ -241,48 +212,48 @@ int crear(){
 	
 	respaldo += nombre + "|" + clave + "|" + fondos + "\n";
 	
-//cout<<endl<<respaldo;exit(1);
+	//cout << endl << respaldo;exit(1);
 
 	ofstream file2("registro.txt");
-	file2<<respaldo;
+	file2 << respaldo;
 	file2.close();
 	
-	cout<<endl;
+	cout << endl;
 	return 0;
 }
 
-int login(){
+int login () {
 	
 	spam_de_puntos(50, 20);
 	mostrar_frase_lentamente(50, "  # Iniciar sesion #  ");
 	spam_de_puntos(50, 20);
-	cout<<endl;
+	cout << endl;
 	
 	string nombre, linea, clave;
 	
-//Nombre.
-	cout<<"Nombre de usuario: ";
-	cin>>nombre;
-	if(!comprobar(1, nombre)){
+	//Nombre.
+	cout << "Nombre de usuario: ";
+	cin >> nombre;
+	if (!comprobar(1, nombre)) {
 		
-		cout<<"Nombre invalido."<<endl;
+		cout << "Nombre invalido." << endl;
 		return 0;
 	}
 	
-//Clave.
+	//Clave.
 	
 	ifstream file("registro.txt");
-	while(getline(file, linea)){
+	while (getline(file, linea)) {
 		
 		if(nombre == extraer(1, linea))break;
 	}
 	file.close();
 	
-	cout<<"Clave: ";
-	cin>>clave;
-	if(clave != extraer(2, linea)){
+	cout << "Clave: ";
+	cin >> clave;
+	if (clave != extraer(2, linea)) {
 		
-		cout<<"Clave invalida."<<endl;
+		cout << "Clave invalida." << endl;
 		return 0;
 	}
 	
@@ -293,16 +264,17 @@ int login(){
 	spam_de_puntos(20, 10);
 	founds = stof(extraer(3, linea));
 	
-	cout<<endl<<"Usuario: "<<extraer(1, linea)<<endl;
-	cout<<"Clave: "<<extraer(2, linea)<<endl;
-	cout<<extraer(3, linea)<<endl;
+	cout << endl << "Usuario: " << extraer(1, linea) << endl;
+	cout << "Clave: " << extraer(2, linea) << endl;
+	cout << extraer(3, linea) << endl;
 	
 	menu_user_logged();
 	
 	return 0;
 }
 
-int main(){menu_user_logged();
+int main () {
+	menu_user_logged();
 	
 	int select; string opciones;
 	
@@ -310,14 +282,14 @@ int main(){menu_user_logged();
 	opciones += "2) Crear cuenta.\n";
 	opciones += "3) Salir.\n";
 	
-	while(true){//Menu de inicio.
+	while (true) {//Menu de inicio.
 		
-		cout<<"\t\t";
+		cout << "\t\t";
 		mostrar_frase_lentamente(30, "Bienvenido al Banco Petare");
-		cout<<endl<<endl;
+		cout << endl << endl;
 		
 		mostrar_frase_lentamente(30, opciones);
-		cin>>select;
+		cin >> select;
 		
 		switch(select){
 			
@@ -326,7 +298,7 @@ int main(){menu_user_logged();
 			case 3: return 0;
 			
 			default:
-			cout<<"Chingada madre, escribe las opciones que te doy, puto animal."<<endl;
+			cout << "Chingada madre, escribe las opciones que te doy, puto animal." << endl;
 			break;
 		}
 	}
